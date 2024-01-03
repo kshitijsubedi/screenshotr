6d8d5fdd79396d99661c20de9f9eb19c3d572584
Kshitij Subedi <2subedikshitij@gmail.com>
2024-01-03 22:04:24 +0545

    Update dependencies and server configuration

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 import axios from "axios";
-import Fastify from "fastify";
+import bodyParser from "body-parser";
+import express from "express";
 import captureWebsite from "capture-website";
 import "dotenv/config";
 
-const fastify = Fastify({
-  logger: true,
-});
+const app = express();
+app.use(bodyParser.json());
+
 // Declare a route
-fastify.get("/", async function handler(request, reply) {
-  return { hello: "world" };
+app.get("/", async function handler(request, reply) {
+  reply.json({ hello: "world" });
 });
 
-fastify.post("/screenshot", async function handler(request, reply) {
+app.post("/screenshot", async function handler(req, res) {
+  const { url } = req.body;
+  if (!url) return res.send("No url provided");
+
   const form = new FormData();
   form.set("key", process.env.IMG_API);
-  const { url } = request.body;
   try {
+    console.log("Capturing: ", url);
     const screenshot = await captureWebsite.base64(url, {
       delay: 5,
+      launchOptions: {
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        headless: "new",
+      },
     });
     form.append("image", screenshot);
     const { data } = await axios.post(process.env.IMG_URL, form);
-    return data.data.url;
+    res.send(data.data);
   } catch (e) {
     if (axios.isAxiosError(e)) {
       const error = e;
-      return error.response?.data;
-    } else reply.code(500);
+      res.send(error.response?.data);
+    } else res.send(e);
   }
 });
 
 // Run the server!
-try {
-  await fastify.listen({ port: process.env.PORT || 3000 });
-} catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
-}
+
+app.listen(process.env.PORT || 3000, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${process.env.PORT || 3000}`);
+});
